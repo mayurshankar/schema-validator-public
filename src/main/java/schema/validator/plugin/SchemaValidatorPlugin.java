0b087919f5d946b660ca9761771c80779e5db37d
Mayur Shankar <mayur.shankar@lloydsbanking.dev>
2023-12-08 16:37:30 +0000

    Setting up dependencies

--- src/main/java/schema/validator/plugin/SchemaValidatorPlugin.java
+++ src/main/java/schema/validator/plugin/SchemaValidatorPlugin.java
@@ -2,4 +2,8 @@ package schema.validator.plugin;
 
 import io.cdap.cdap.api.annotation.Description;
 public class SchemaValidatorPlugin {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
 }
