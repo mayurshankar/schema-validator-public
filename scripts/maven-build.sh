0509b44241bbce39bc2c3ce1a034768aa7ab1aba
Mayur Shankar <mayur.shankar@lloydsbanking.dev>
2023-12-13 20:47:49 +0000

    Add frequent maven commands

---
+++ scripts/maven-build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Installs all dependencies from remote
+reload-remote-deps () {
+  mvn dependecy:purge-local-repository
+}
+
+build-and-test () {
+  mvn clean package
+}
+
+# Only uses remote snapshots
+build-and-test-full () {
+  mvn clean package -U
+}
+
+build-and-skip-test () {
+  mvn clean package -DskipTests
+}
