9dc63b839fd13cde6c421055fd58c22fda014885
Mayur Shankar <mayur.shankar@lloydsbanking.dev>
2024-01-16 15:12:13 +0000

    WIP automate uploading to CDF

--- scripts/bigquery.sh
+++ scripts/bigquery.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 
+project_id="playpen-fbeec3"
+
 download-schema () {
 	#bq show --schema --format=json playpen-e3d014:test_source_data.Integers > /Users/mayur.shankar/Documents/schema-validator/src/test/resources/bq-schemas/Integers-Schema.json
 	
-	bq show --schema --format=json playpen-e3d014:test_source_data.Integers > /Users/mayur.shankar/Documents/schema-validator/src/test/resources/schemas/bigquery/test_source_data/Integers.json
+	bq show --schema --format=json $project_id:test_source_data.Integers > /Users/mayur.shankar/Documents/schema-validator/src/test/resources/schemas/bigquery/test_source_data/Integers.json
 }
 
 upload-schema () {
-  bq load --source_format=NEWLINE_DELIMITED_JSON playpen-e3d014:data_schemas.Schema1 /Users/mayur.shankar/Documents/schema-validator/src/test/resources/schemas/bigquery/test_source_data/Integers.jsonl mode:STRING,type:STRING,name:STRING
+  bq load --source_format=NEWLINE_DELIMITED_JSON $project_id:data_schemas.Schema1 /Users/mayur.shankar/Documents/schema-validator/src/test/resources/schemas/bigquery/test_source_data/Integers.jsonl mode:STRING,type:STRING,name:STRING
 
 }
 
+upload-data () {
+  bq load --autodetect --source_format=CSV $project_id:input_dataset.ingested_data ../src/test/resources/local-source/source-int.csv
+}
+
 #download-schema
-upload-schema
+#upload-schema
+upload-data
---
+++ terraform/main.tf
@@ -0,0 +1,62 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.51.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file("../../keys/playpen-fbeec3.json")
+
+  project = "playpen-fbeec3"
+  region  = "europe-west2"
+}
+
+// Cloud Data Fusion Basic Instance
+resource "google_data_fusion_instance" "basic_instance" {
+  name                          = "schema-validation"
+  region                        = "europe-west2"
+  type                          = "BASIC"
+  version                       = "6.9.2"
+  enable_stackdriver_logging    = true
+  enable_stackdriver_monitoring = false
+  private_instance              = false
+}
+
+// Big Query Dataset & Table (Ingested files)
+resource "google_bigquery_dataset" "input-dataset" {
+  dataset_id = "input_dataset"
+  location   = "europe-west2"
+}
+/*
+resource "google_bigqery_table" "input-table" {
+  dataset_id = google_bigquery_dataset.input-dataset.dataset_id
+  table_id   = "ingested-data"
+}
+*/
+
+// Big Query Dataset & Table (Schemas)
+resource "google_bigquery_dataset" "schema-dataset" {
+  dataset_id = "schema_dataset"
+  location   = "europe-west2"
+}
+/*
+resource "google_bigquery_table" "schema-table" {
+  dataset_id = google_bigquery_dataset.schema-dataset.dataset_id
+  table_id   = "schemas"
+}
+*/
+
+// Big Query Dataset & Table (Outputted files)
+resource "google_bigquery_dataset" "output-dataset" {
+  dataset_id = "output_dataset"
+  location   = "europe-west2"
+}
+/*
+resource "google_bigquery_table" "output-table" {
+  dataset_id = google_bigquery_dataset.output-dataset.dataset_id
+  table_id   = "outputted-data"
+}
+*/
